9c2a2e50ddceebbb263f9fdecf3247db65e9f944
Yusuf ÖZTÜRK <ysfoztrk37343@gmail.com>
2026-05-09 22:06:09 +0300

    Add files via upload

---
+++ HAREKET_TAK_P_S_STEM_/HAREKET_TAK_P_S_STEM_.ino
@@ -0,0 +1,14 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
